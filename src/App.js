c20f64ade59e4bf75e2ca73be90cd358e393b2f6
Mustafiz Rahman <62724969+mustafizr386@users.noreply.github.com>
2023-09-29 20:13:53 -0400

    lots

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import './App.css';
 
+
+import Sidebar from './experiments/Sidebar';
+import Banner from './experiments/Banner';
+import Portfolio from './experiments/Home';
+import AboutMe from './experiments/About Me';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+	
+        <div className="App">
+            <Banner/>
+            <Portfolio className="Portfolio" />
+            <AboutMe/>
+            <Sidebar/>
+
+        </div>
+    );
+  
 }
+
 
 export default App;
